1464722289cd2f1678d64999cdc9bed600b832c2
LemonMantis5571 <briter456@gmail.com>
2023-02-24 16:34:23 -0600

    Renombrando variables

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ export const pool = createPool({
     host: process.env.HOST || "",
     user: process.env.USER || "",
     password: process.env.PASSWORD || "",
-    port: process.env.PORT || "",
+    port: process.env.DB_PORT || "",
     database: process.env.DATABASE || "",
 
 });
