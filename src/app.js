749f88d4c9afc36cf040a817e38634f28f4d32e7
LemonMantis5571 <briter456@gmail.com>
2023-02-25 23:29:11 -0600

    Encabezados

--- src/app.js
+++ src/app.js
@@ -9,6 +9,15 @@
 
     app.set('port', process.env.PORT);
 
+    //header
+
+    app.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        next();
+    });
+
     // Routes
     app.use(express.json());
 
