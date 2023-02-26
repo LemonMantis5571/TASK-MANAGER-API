6ac5594ab0dc5e5458b510aa443d5d5c1f425972
LemonMantis5571 <briter456@gmail.com>
2023-02-26 15:10:59 -0600

    Manejo de errores

--- src/Auth/auth.token.js
+++ src/Auth/auth.token.js
@@ -3,14 +3,13 @@ import jwt from 'jsonwebtoken';
 
 export default function verifyToken(req, res, next) {
     const token = req.headers.authorization.split(' ')[1];
-    console.log(token);
 
     if(!token) {
         return res.status(401).send({message: 'Missing auth token'});
     }
 
     try {
-        const decoded = jwt.verify(token, 'secret');
+        const decoded = jwt.verify(token, process.env.APIKEY);
         req.userId = decoded.userId;
         console.log(req.userId);
         next();
