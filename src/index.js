042b2506de26faf41eb65c687914278c8a2e93be
LemonMantis5571 <briter456@gmail.com>
2023-04-23 23:33:54 -0600

    AÑADIENDO HOME A INDEX.JS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import app from "./app.js";
 const home = require("./routes/home");
 const main = () => {
+    app.use("/home", home);
     app.listen(app.get("port"))
     console.log(`Server on port ${app.get('port')}`);
 };
