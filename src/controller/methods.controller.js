ec94dda9aeb7c4a077cf613325e1d2b9a7d83d40
LemonMantis5571 <briter456@gmail.com>
2023-04-23 21:17:52 -0600

    Migración a google cloud

--- src/controller/methods.controller.js
+++ src/controller/methods.controller.js
@@ -1,8 +1,7 @@
-import { getUser, UpdateUsers, createUser, DeleteUsers, getUserByID, loginUsers} from "./users.controller.js";
+import { UpdateUsers, createUser, DeleteUsers, getUserByID, loginUsers} from "./users.controller.js";
 import { DeleteUserTasks, getUserTasks, CreateUserTask } from "./database.controller.js";
 
 export const methods = {
-    getUser,
     UpdateUsers,
     createUser,
     DeleteUsers,
